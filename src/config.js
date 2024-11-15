5055e368c8299f7ac3bf0683cecf7ddcf2f5fd63
gustavo <gustavoaguirregomez10@gmail.com>
2024-11-15 01:06:13 -0300

    36

--- src/config.js
+++ src/config.js
@@ -4,5 +4,3 @@ dotenv.config()
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const PORT = process.env.PORT || 4000;
 export const SECRET_KEY = process.env.SECRET_KEY;
-console.log(process.env.SECRET_KEY)
-console.log(process.env.PORT)
