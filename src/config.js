cb2ce39038069f6968b9345b58e795125e7692c1
gustavo <gustavoaguirregomez10@gmail.com>
2024-11-15 13:51:28 -0300

    22

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const MONGODB_URI = process.env.MONGODB_URI;
-export const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 export const SECRET_KEY = process.env.SECRET_KEY;
